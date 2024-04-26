7147c1f0cc130aa93b3a355d898cb196cea7f693
cfo-pass <linuxneodo@outlook.com>
2024-04-27 02:10:59 +0800

    first commit

---
+++ docs/.vuepress/config.js
@@ -0,0 +1,48 @@
+import { hopeTheme } from 'vuepress-theme-hope';
+import { defineUserConfig } from 'vuepress/cli'
+import { viteBundler } from '@vuepress/bundler-vite'
+import { searchProPlugin } from "vuepress-plugin-search-pro";
+
+export default defineUserConfig({
+  base:'/docs/',
+  lang: 'zh-CN',
+
+  title: 'API文档',
+  description: 'API文档',
+  
+  head: [['link', { rel: 'icon', href: '' }]],
+
+  theme: hopeTheme({
+    logo: 'https://vuejs.press/images/hero.png',
+
+    navbar: [
+      {text: 'vuepress', link: '/vuepress/'}
+    ],
+
+    sidebar: {
+      '/vuepress/': "structure",
+    },
+
+    plugins: {
+      // comment: {
+      //   provider: 'Giscus',
+      //   repo: '', 
+      //   repoId: '',
+      //   category: 'Announcements',
+      //   categoryId: '',
+      //   position:'bottom',
+      // },
+      components: {
+        components: ["BiliBili", "VidStack"],
+      },
+      searchPro: {
+        searchMaxSuggestions: 10,
+        searchPlaceholder: '搜索文档',
+      }
+    },
+ 
+  }),
+
+  bundler: viteBundler(),
+
+})

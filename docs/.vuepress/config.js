469102c0bf12c5676eeb14146e26bf5021c0940f
cfo-pass <162004147+cfo-pass@users.noreply.github.com>
2024-04-30 21:37:28 +0800

    Update config.js

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -15,7 +15,6 @@ module.exports = defineUserConfig({
   ],
 
   theme: hopeTheme({
-    // logo: 'https://n.sinaimg.cn/sinacn14/300/w1620h1080/20180702/7ec4-hespqry6080115.jpg',
 
     navbar: [
       { text: 'vuepress', link: '/vuepress/' },

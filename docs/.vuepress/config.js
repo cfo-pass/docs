cbb8fab1a4cac29fffb3b9be628d5b6fa1f3d98a
cfo-pass <162004147+cfo-pass@users.noreply.github.com>
2024-04-30 19:21:00 +0800

    Update config.js

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -16,7 +16,8 @@ export default defineUserConfig({
     logo: 'https://n.sinaimg.cn/sinacn14/300/w1620h1080/20180702/7ec4-hespqry6080115.jpg',
 
     navbar: [
-      {text: 'vuepress', link: '/vuepress/'}
+      {text: 'vuepress', link: '/vuepress/'},
+      {text: 'pandora', link: '/pandora/'},
     ],
 
     sidebar: {

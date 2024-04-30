ce090368e7f0e1548f934de4739329a3638e4b9e
cfo-pass <162004147+cfo-pass@users.noreply.github.com>
2024-04-30 22:46:27 +0800

    Update config.js

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -22,7 +22,7 @@ export default defineUserConfig({
       {
         text: 'Google',
         link: 'https://google.com',
-        icon: 'mingcute:qq-fill'
+        icon: 'flat-color-icons:google'
       }
     ],
 

b93ea2504ae7e7b49030c5c2d0c768c8b6a90642
cfo-pass <162004147+cfo-pass@users.noreply.github.com>
2024-04-30 21:33:19 +0800

    Update config.js

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,27 +1,25 @@
-import { hopeTheme } from 'vuepress-theme-hope';
-import { defineUserConfig } from 'vuepress/cli'
-import { viteBundler } from '@vuepress/bundler-vite'
-import { searchProPlugin } from "vuepress-plugin-search-pro";
+const { hopeTheme } = require('vuepress-theme-hope');
+const { defineUserConfig } = require('@vuepress/cli');
+const { viteBundler } = require('@vuepress/bundler-vite');
+const { searchProPlugin } = require("vuepress-plugin-search-pro");
 
-export default defineUserConfig({
-  base:'/docs/',
+module.exports = defineUserConfig({
+  base: '/docs/',
   lang: 'zh-CN',
-
   title: 'API文档',
-  description: 'API文档',
-  
+  description: '详尽的API文档',
+
   head: [
-    ['link', { rel: 'stylesheet', href: 'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css' }],
+    ['link', { rel: 'stylesheet', href: 'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css' }],  // 确保FontAwesome库被加载
     ['link', { rel: 'icon', href: 'https://n.sinaimg.cn/sinacn14/300/w1620h1080/20180702/7ec4-hespqry6080115.jpg' }]
   ],
-
 
   theme: hopeTheme({
     logo: 'https://n.sinaimg.cn/sinacn14/300/w1620h1080/20180702/7ec4-hespqry6080115.jpg',
 
     navbar: [
-      {text: 'vuepress', link: '/vuepress/'},
-      {text: 'pandora', link: '/pandora/'},
+      { text: 'vuepress', link: '/vuepress/' },
+      { text: 'pandora', link: '/pandora/' },
       {
         text: 'Google',
         link: 'https://google.com',
@@ -35,25 +33,12 @@ export default defineUserConfig({
     },
 
     plugins: {
-      comment: {
-        provider: 'Giscus',
-        repo: 'cfo-pass/docs', 
-        repoId: 'R_kgDOLzklDw',
-        category: 'Announcements',
-        categoryId: 'DIC_kwDOLzklD84Ce--n',
-        position:'bottom',
-      },
-      components: {
-        components: ["BiliBili", "VidStack"],
-      },
-      searchPro: {
+      searchPro: searchProPlugin({
         searchMaxSuggestions: 10,
         searchPlaceholder: '搜索文档',
-      }
+      })
     },
- 
   }),
 
   bundler: viteBundler(),
-
-})
+});
